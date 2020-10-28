f4f3898e7e23baa1f15e23f5561bf76abd8ebea9
zhujintao-kzx <983068902@qq.com>
2020-10-28 17:18:55 +0800

    commit message

---
+++ demo-zjt/src/main/java/com/taotao/study/demo/zjt/Application.java
@@ -0,0 +1,16 @@
+package com.taotao.study.demo.zjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * my project
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ demo-zjt/src/test/java/com/taotao/study/demo/zjt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taotao.study.demo.zjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
